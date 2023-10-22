a134555311e312675969522d7f7baf7e9abfc0e4
aakash bondwal <aakashbondwal4@gmail.com>
2023-10-22 19:53:31 +0530

    Initial Commit

---
+++ ClimateEye/ClimateEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateEyeApp.swift
+//  ClimateEye
+//
+//  Created by Aakash  Bondwal  on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ClimateEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
